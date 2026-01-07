03ff4fbd567ee277388e15e0bf9b68026d67b98e
DariaBoitsova <dariaboitsova1@gmail.com>
2026-01-07 11:28:06 +0300

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,23 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def build_features(cpm, channel, date, channel_stats):
-    date = pd.to_datetime(date)
-    stats = channel_stats.get(channel, channel_stats["__global__"])
+def build_features(CPM, CHANNEL_NAME, DATE, channel_stats):
+    DATE = pd.to_datetime(DATE)
+    stats = channel_stats.get(CHANNEL_NAME, channel_stats["__global__"])
 
     features = {
-        "CPM": cpm,
-        "cpm_log": np.log1p(cpm),
-        "cpm_sq": cpm ** 2,
-        "dayofweek": date.dayofweek,
-        "week": date.isocalendar().week,
-        "month": date.month,
-        "is_weekend": int(date.dayofweek in [5, 6]),
+        "CPM": CPM,
+        "cpm_log": np.log1p(CPM),
+        "cpm_sq": CPM ** 2,
+        "dayofweek": DATE.dayofweek,
+        "week": DATE.isocalendar().week,
+        "month": DATE.month,
+        "is_weekend": int(DATE.dayofweek in [5, 6]),
         "channel_mean_views": stats["mean"],
         "channel_median_views": stats["median"],
         "channel_history_count": stats["count"],
-        "cpm_vs_channel_mean": cpm / stats["mean_cpm"],
-        "CHANNEL_NAME": channel
+        "cpm_vs_channel_mean": CPM / stats["mean_cpm"],
+        "CHANNEL_NAME": CHANNEL_NAME
     }
 
     return pd.DataFrame([features])
